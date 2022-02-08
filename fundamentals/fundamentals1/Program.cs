22fd4bc857f914ddac65992d971eb3e34d6c0872
Drake Sanders <drake_sanders12@icloud.com>
2022-02-07 22:35:24 -0600

    Fundamentals1 commit

---
+++ fundamentals/fundamentals1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace fundamentals1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            printTo255();
+            divisible();
+        }
+
+        public static void printTo255()
+        {
+            for(int i=0;i<256;i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void divisible()
+        {
+            for(int i=0;i<101;i++)
+            {
+                if(i%5==0 && i%3==0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i%5==0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i%3==0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+            }
+        }
+    }
+}
