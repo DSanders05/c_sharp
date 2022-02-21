739eca54832219832db1aaa1eaf4da793fec6914
Drake Sanders <drake_sanders12@icloud.com>
2022-02-21 15:22:37 -0600

    Login and Reg Commit

---
+++ orm_intro/log_reg/login_registration/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using login_registration.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+
+namespace login_registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MyContext _context;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, MyContext dbContext)
+        {
+            _logger = logger;
+            _context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(User newUser)
+        {
+            if(ModelState.IsValid)
+            {
+                if(_context.Users.Any(u=>u.Email == newUser.Email))
+                {
+                    ModelState.AddModelError("Email", "Email already in use!");
+                    return View("Index");
+                }
+                PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+                _context.Users.Add(newUser);
+                _context.SaveChanges();
+                return RedirectToAction("Success");
+            } else {
+                return View("Index");
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LogUser logUser)
+        {
+            if(ModelState.IsValid)
+            {
+                User userInDB = _context.Users.FirstOrDefault(s=>s.Email == logUser.LEmail);
+                if(userInDB == null)
+                {
+                    ModelState.AddModelError("LEmail", "Invalid login attempt");
+                    return View("Index");
+                }
+                PasswordHasher<LogUser> Hasher = new PasswordHasher<LogUser>();
+                PasswordVerificationResult result = Hasher.VerifyHashedPassword(logUser, userInDB.Password, logUser.LPassword);
+                if(result == 0)
+                {
+                    ModelState.AddModelError("LEmail", "Invalid login attempt");
+                    return View("Index");
+                }
+                HttpContext.Session.SetInt32("UserId", userInDB.UserId);
+                return RedirectToAction("Success");
+            } else {
+                return View("Index");
+            }
+        }
+
+        [HttpGet("Success")]
+        public IActionResult Success()
+        {
+            int? validUser = HttpContext.Session.GetInt32("UserId");
+            if(validUser == null)
+            {
+                ModelState.AddModelError("LEmail", "You must first login to access web pages.");
+                return View("Index");
+            }
+            return View();
+        }
+
+        [HttpGet("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
